86f390cae8b0dbdf42bc15b1dfa1ad2bf6e57743
ngminhthanh12a3 <ngminhthanh12a3@gmail.com>
2023-06-20 20:57:48 +0700

    1.3.0
    Send a frame requiring assigning a virtual storage.

--- Core/Src/DESIoT_device.c
+++ Core/Src/DESIoT_device.c
@@ -21,7 +21,50 @@ void DESIOT_Rx1byte(uint8_t rxByte)
 
 }
 
+/**
+ * Send a data packet requiring to assign an integer to the gateway.
+ */
 void DESIoT_assignInt(uint8_t VS, size_t integer)
+{
+	uint8_t dataPacket[DESIOT_CMD_LEN + DESIOT_DATALEN_LEN + DESIOT_ASSIGN_INT_PKG_LEN] = {0};
+
+	DESIoT_dataPacket_t *pDataPacket = (DESIoT_dataPacket_t*)dataPacket;
+
+	pDataPacket->cmd = DESIOT_CMD_ASSIGN_VIRTUAL_STORAGE;
+	pDataPacket->dataLen = sizeof(integer);
+
+	uint8_t *data = (dataPacket + DESIOT_CMD_LEN + DESIOT_DATALEN_LEN);
+	data[0] = VS; // assign VS
+	memcpy(data + 1, &integer, sizeof(integer)); // assign integer
+
+	DESIoT_sendDataPacket(sizeof(dataPacket), dataPacket);
+}
+
+
+/**
+ *
+ */
+void DESIoT_sendDataPacket(const size_t dataLen, uint8_t *data)
+{
+	uint8_t frame[DESIOT_HEADERS_LEN + dataLen + DESIOT_CRC_LEN];
+	memset(frame, 0, sizeof(frame));
+
+	DESIoT_headFrame_t *headFrame = (DESIoT_headFrame_t*)frame;
+	DESIoT_trailFrame_t *trailFrame = (DESIoT_trailFrame_t*)(frame + sizeof(frame) - (DESIOT_TRAIL_LEN + DESIOT_CRC_LEN));
+
+	headFrame->h1 = DESIOT_H1_DEFAULT;
+	headFrame->h2 = DESIOT_H2_DEFAULT;
+	trailFrame->t1 = DESIOT_T1_DEFAULT;
+	trailFrame->t2 = DESIOT_T2_DEFAULT;
+
+	// set data packet to frame
+	memcpy(frame + DESIOT_HEAD_LEN, data, dataLen);
+
+	trailFrame->crc = 0;
+	DESIOT_SENDBYTES_F_NAME(sizeof(frame), frame);
+}
+
+DESIOT_ATT_WEAK DESIOT_SENDBYTES
 {
 
 }
